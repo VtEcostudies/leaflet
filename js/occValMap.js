9d487933711e53cdee84b3100377b92fdb859a3c
jloomisVCE <jtloomis@gmail.com>
2018-10-24 23:37:38 -0400

    modified:   js/occValMap.js 	modified:   val_speciespage_leaflet.html

--- js/occValMap.js
+++ js/occValMap.js
@@ -9,22 +9,15 @@ import {speciesList} from "./map_these_species.js";
 
 var vceCenter = [43.6962, -72.3197]; //VCE coordinates
 var vtCenter = [43.916944, -72.668056]; //VT geo center, downtown Randolph
-var cmLayer = []; //an array of circleMarker 'layers' to keep track of for removal and deletion
-var cmIndex = 0; //a global counter for cmLayer array-objects
-var cmCount = 0; //a global counter for cmLayer array-objects across mutiple species
-var cmColor = {select:0, options:[
-                    {index:0, color:"blue", radius:3},
-                    {index:1, color:"red", radius:3},
-                    {index:2, color:"green", radius:3},
-                    {index:2, color:"yellow", radius:3},
-                    {index:2, color:"purple", radius:3}
-                ]
-            };
+var cmGroup = {}; //object of layerGroups of different species' markers grouped into layers
+var cmCount = {}; //a global counter for cmLayer array-objects across mutiple species
+var cgColor = {}; //object of colors for species layers
+var colors = {0:"red",1:"blue",2:"green",3:"yellow",4:"purple",5:"grey"};
 var myMap = {};
 var myRenderer = L.canvas({ padding: 0.5 }); //make global so we can clear the canvas before updates...
 var xhrRecsPerPage = 500; //the number of records to load per ajax request.  more is faster.
 var vtWKT = "POLYGON((-73.3427 45.0104,-73.1827 45.0134,-72.7432 45.0153,-72.6100 45.0134,-72.5551 45.0075,-72.4562 45.0090,-72.3113 45.0037,-72.0964 45.0066,-71.9131 45.0070,-71.5636 45.0138,-71.5059 45.0138,-71.5294 44.9748,-71.4949 44.9123,-71.5567 44.8296,-71.6281 44.7506,-71.6061 44.7077,-71.5677 44.6481,-71.5388 44.5817,-71.6006 44.5533,-71.5746 44.5308,-71.5883 44.4955,-71.6556 44.4504,-71.7146 44.4093,-71.7957 44.3975,-71.8163 44.3563,-71.8698 44.3327,-71.9138 44.3484,-71.9865 44.3386,-72.0346 44.3052,-72.0428 44.2432,-72.0662 44.1930,-72.0360 44.1349,-72.0580 44.0698,-72.1101 44.0017,-72.0937 43.9671,-72.1252 43.9088,-72.1733 43.8682,-72.1994 43.7899,-72.1994 43.7899,-72.2392 43.7384,-72.3010 43.7056,-72.3271 43.6391,-72.3436 43.5893,-72.3793 43.5814,-72.3972 43.5027,-72.3807 43.4988,-72.3999 43.4150,-72.4123 43.3601,-72.3903 43.3591,-72.4081 43.3282,-72.3999 43.2762,-72.4370 43.2342,-72.4493 43.1852,-72.4480 43.1311,-72.4507 43.0679,-72.4438 43.0067,-72.4699 42.9846,-72.5276 42.9645,-72.5331 42.8951,-72.5633 42.8639,-72.5098 42.7863,-72.5166 42.7652,-72.4741 42.7541,-72.4590 42.7289,-73.2761 42.7465,-73.2912 42.8025,-73.2850 42.8357,-73.2678 43.0679,-73.2472 43.5022,-73.2561 43.5615,-73.2939 43.5774,-73.3049 43.6271,-73.3557 43.6271,-73.3976 43.5675,-73.4326 43.5883,-73.4285 43.6351,-73.4079 43.6684,-73.3907 43.7031,-73.3516 43.7701,-73.3928 43.8207,-73.3832 43.8533,-73.3969 43.9033,-73.4086 43.9365,-73.4134 43.9795,-73.4381 44.0427,-73.4141 44.1058,-73.3928 44.1921,-73.3427 44.2393,-73.3186 44.2467,-73.3406 44.3484,-73.3385 44.3690,-73.2946 44.4328,-73.3296 44.5367,-73.3832 44.5919,-73.3770 44.6569,-73.3681 44.7477,-73.3317 44.7857,-73.3324 44.8043,-73.3818 44.8398,-73.3564 44.9040,-73.3392 44.9181,-73.3372 44.9643,-73.3537 44.9799,-73.3447 45.0046,-73.3447 45.0109,-73.3426 45.0104,-73.3427 45.0104))";
-window.valXHR = new XMLHttpRequest();
+//window.valXHR = new XMLHttpRequest();
 
 //for standalone use
 function addMap() {
@@ -49,14 +42,10 @@ function addMap() {
 function initValOccCanvas() {
     console.log(`initValOccCanvas()`);
     cmCount = 0;
-    //remove all circleMarkers
-    cmLayer.forEach(function(cm, index) {
-        cm.remove(); //remove the marker from the map
-        delete cmLayer[index]; //delete it from our index of markers
-        cmIndex--;
-        if (cmLayer.length < 1) {
-            cmIndex = 0;
-        }
+    //remove all circleMarkers from each group by clearing the layer
+    Object.keys(cmGroup).forEach(function(key){
+        console.log(`Clear layer '${key}'`);
+        cmGroup[key].clearLayers(); 
     });
 }
 
@@ -69,8 +58,6 @@ function initValOccCanvas() {
 function addValOccCanvas(taxonName=null) {
     
     console.log(`addValOccCanvas(${taxonName})`);
-    
-    if (taxonName) {alert(taxonName);}
     
     var baseUrl = 'http://vtatlasoflife.org/biocache-service/occurrences/search?q='; // biocache-service/occurrences/search?q=*:*
     var pageSize = `&pageSize=${xhrRecsPerPage}`;
@@ -85,29 +72,30 @@ function addValOccCanvas(taxonName=null) {
     var valUrl = baseUrl + taxonName + pageSize + wkt;
     if(document.getElementById("apiUrlLabel")) {document.getElementById("apiUrlLabel").innerHTML = (valUrl);}
 
-    if (cmColor.select < cmColor.options.length) {cmColor.select++;} else {cmColor.select=0;}
-
     // abort pending requests and start a new chain
-    initXhrRequest(valUrl);
+    initXhrRequest(valUrl, taxonName);
 }
 
-function initXhrRequest(url) {
-
-    // abort any pending requests
-    valXHR.abort();
+function initXhrRequest(url, taxonName) {
+    var valXHR = new XMLHttpRequest();
+    
+    // abort any pending requests on the global handle
+    //valXHR.abort();
+    
+    console.log(`initXhrRequest(${url})`);
 
     //handle request responses here in this callback
     valXHR.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            xhrResults(this, url);
+            xhrResults(this, url, taxonName);
         }
     };
     
     //load the first N records of data, which initiates subsequent page loads
-    loadPage(url, 0);
+    loadPage(valXHR, url, taxonName, 0);
 }
 
-function loadPage(url, startIndex) {
+function loadPage(valXHR, url, taxonName, startIndex) {
     valXHR.open("GET", url+`&startIndex=${startIndex}`, true);
     /*
      * NOTE: the VAL ala-demo site does not respond with a "Content-type" header.  We only get an "x-requested-with" pre-flight header
@@ -120,7 +108,7 @@ function loadPage(url, startIndex) {
     valXHR.send();    
 }
 
-function xhrResults(valXHR, url) {
+function xhrResults(valXHR, url, taxonName) {
     var jsonRes = JSON.parse(valXHR.response);
     var totr = jsonRes.totalRecords;
     var perp = jsonRes.pageSize;
@@ -129,40 +117,45 @@ function xhrResults(valXHR, url) {
     if (netr > totr) {netr = totr;}
     if (document.getElementById("apiUrlLabel")) {document.getElementById("apiUrlLabel").innerHTML = (url+`&startIndex=${startIdx}`);}
     if (document.getElementById("jsonResults")) {
-        document.getElementById("jsonResults").innerHTML = `total records: ${totr}  |  records loaded: ${netr}`;
+        document.getElementById("jsonResults").innerHTML = `total ${taxonName}: ${totr}  | ${taxonName} loaded: ${netr}`;
         }
-    updateMap(jsonRes.occurrences);
+    updateMap(jsonRes.occurrences, taxonName);
     if (totr > netr) {
-        loadPage(url, netr+1, xhrResults);
+        loadPage(valXHR, url, taxonName, netr+1);
     }
 }
 
-function occurrencePopupInfo(occRecord) {
-    var info = '';
-    Object.keys(occRecord).forEach(function(key) {
-        info += `${key}: ${occRecord[key]}` + String.fromCharCode(13);
-    });
-    return info;    
-}
-
-function updateMap(valJsonData) {
-
+function updateMap(valJsonData, taxonName) {
+    console.log(`updateMap(${valJsonData[0].species})`);
     for (var i = 0; i < valJsonData.length; i++) {
         if (!valJsonData[i].decimalLatitude || !valJsonData[i].decimalLongitude) {
             console.log(`WARNING: Occurrence Record without Lat/Lon values: ${valJsonData[i]}`);
             return;
         }
-        cmCount++;
+        cmCount[taxonName]++;
+        cmCount['all']++;
         var llLoc = L.latLng(valJsonData[i].decimalLatitude, valJsonData[i].decimalLongitude);
-        cmLayer[cmIndex++] = L.circleMarker(llLoc, {
+        cmGroup[taxonName].addLayer(L.circleMarker(llLoc, {
             renderer: myRenderer,
-            radius: cmColor.options[cmColor.select].radius,
-            color: cmColor.options[cmColor.select].color
-          }).addTo(myMap).bindPopup(occurrencePopupInfo(valJsonData[i]));
+            radius: 3,
+            color: cgColor[taxonName]
+          }).bindPopup(occurrencePopupInfo(valJsonData[i])));
     }
+    
     if (document.getElementById("jsonResults")) {
-        document.getElementById("jsonResults").innerHTML += ` | records mapped: ${cmLayer.length} | ${cmCount}`;
+        document.getElementById("jsonResults").innerHTML += ` | records mapped: ${cmCount['all']}`;
     }
+}
+
+function occurrencePopupInfo(occRecord) {
+    var info = '';
+    
+    Object.keys(occRecord).forEach(function(key) {
+        if (['scientificName','occurrenceID','dataProviderName','dataResourceName','Collector','basisOfRecord','date','year','month','day'].includes(key)) {
+            info += `${key}: ${occRecord[key]}<br/>`;
+        }
+    });
+    return info;    
 }
 
 function addMarker() {
@@ -220,14 +213,45 @@ if (document.getElementById("valLoadOnOpen")) {
         initValStandalone();
         myMap.options.minZoom = 8;
         myMap.options.maxZoom = 12;
-        initValOccCanvas();
+        //initValOccCanvas();
         //addValOccCanvas('Bombus borealis');
-        /*
-         * add multiple species to map, loaded from imported .js file (see top of this file)
-         * must be of the form {"species name": "name type"}
-         */
-        Object.keys(speciesList).forEach(function(key) {
-            addValOccCanvas(key);
-        });
+        getSpeciesListData();
+        
+        // Add a listener to handle the 'clear Data' button click
+        if (document.getElementById("clearData")) {
+            document.getElementById("clearData").addEventListener("click", function() {
+                initValOccCanvas();
+            });
+        }
+        
+        // Add a listener to handle the 'Get Data' button click
+        if (document.getElementById("getData")) {
+            document.getElementById("getData").addEventListener("click", function() {
+                getSpeciesListData();
+            });
+        }
+
     });
+}
+
+/*
+ * add multiple species to map, loaded from imported .js file (see top of this file)
+ * must be of the form {"species name": "name type"}
+ */
+function getSpeciesListData() {
+    cmCount['all'] = 0;
+    var i=0;
+    var species = L.layerGroup([]);
+    Object.keys(speciesList).forEach(function(key) {
+        cmGroup[key] = L.layerGroup([]).addTo(myMap);
+        species.addLayer(cmGroup[key]);
+        cgColor[key] = colors[i++];
+        cmCount[key] = 0;
+        console.log(`Add species group ${key} with color ${cgColor[key]}`);
+        addValOccCanvas(key);
+    });
+    var layers = {
+        "Show Others": species
+    };
+    L.control.layers(cmGroup, layers).addTo(myMap);
 }
