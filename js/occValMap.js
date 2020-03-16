b834850d0dcb4a5671bee4fc398b04f163148e4f
jloomisVCE <jtloomis@gmail.com>
2020-03-16 17:15:39 -0400

    change signup link to WP page

--- js/occValMap.js
+++ js/occValMap.js
@@ -271,7 +271,7 @@ function onEachFeature(feature, layer) {
             var name = feature.properties.BLOCKNAME;
             var link = feature.properties.BLOCKNAME.replace(/( - )|\s+/g,'').toLowerCase();
             links = `<a target="_blank" href="https://s3.us-west-2.amazonaws.com/val.surveyblocks/${link}.pdf">Get ${name} block map</a></br>`;
-            links += `<a target="_blank" href="https://val.vtecostudies.org/survey/ladybeetle/signup?surveyblock=${link}">Signup for ${name}</a>`
+            links += `<a target="_blank" href="https://val.vtecostudies.org/projects/lady-beetle-atlas/signup?surveyblock=${link}">Signup for ${name}</a>`
           }
         }
         if (props) {
