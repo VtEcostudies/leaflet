53cbbd4ac9d4423a3783205cfaf61c3d2daa8948
jloomisVCE <jloomis@vtecostudies.org>
2023-01-29 09:56:11 -0500

    fix empty google sheets row bug for signups

--- js/fetchGoogleSheetsData.js
+++ js/fetchGoogleSheetsData.js
@@ -5,16 +5,20 @@ let defaultSheetIds = {
     "vernacular": '17_e15RB8GgpMVZgvwkFHV8Y9ZgLRXg5Swow49wZsAyQ'
 }
 
+/*
+    Fetch a single Google sheet's data by sheet ID and ordinal sheet number.
+    
+*/
 export async function fetchGoogleSheetData(spreadsheetId=defaultSheetIds.signUps, sheetNumber=0) {
     let apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/?key=${googleApiKey}&includeGridData=true`;
 
     try {
         let res = await fetch(apiUrl);
-        console.log(`fetchGoogleSheetData(${spreadsheetId},${sheetNumber}) RESULT:`, res);
+        console.log(`fetchGoogleSheetData(${spreadsheetId},${sheetNumber}) RAW RESULT:`, res);
         if (res.status > 299) {return res;}
         let json = await res.json();
-        console.log(`fetchGoogleSheetData(${spreadsheetId}) RESULT:`, json);
-        let prop = json.sheets[sheetNumber].properties.title;
+        console.log(`fetchGoogleSheetData(${spreadsheetId}) JSON RESULT:`, json);
+        let prop = json.sheets[sheetNumber].properties;
         let head = json.sheets[sheetNumber].data[0].rowData[0].values;
         let data = json.sheets[sheetNumber].data[0].rowData.slice(1);
         console.log(`Sheet-${sheetNumber} properties:`, prop);
@@ -30,16 +34,19 @@ export async function fetchGoogleSheetData(spreadsheetId=defaultSheetIds.signUps
 export async function getSignups(sheetNumber=0) {
     try {
         let res = await fetchGoogleSheetData(defaultSheetIds.signUps, sheetNumber);
-        console.log('getSignups RESULT:', res);
+        //console.log('getSignups RESULT:', res);
         if (res.status > 299) {return res;}
         let sign = [];
         res.rows.forEach(row => {
-            sign[row.values[1].formattedValue] = {
-                'last':row.values[4].formattedValue, 
-                'first':row.values[3].formattedValue,
-                'date':row.values[0].formattedValue
+            if (row.values[1]) { //if sheet's row values are deleted but row is not deleted, API returns row of empty values!!!
+                sign[row.values[1].formattedValue] = {
+                    'last':row.values[4].formattedValue, 
+                    'first':row.values[3].formattedValue,
+                    'date':row.values[0].formattedValue
+                }
             }
     })
+    console.log('getSignups 2D ARRAY', sign);
     return sign;
     } catch(err) {
         console.log(`getSignups(${sheetNumber}) ERROR:`, err);
@@ -50,24 +57,34 @@ export async function getSignups(sheetNumber=0) {
 export async function getVernaculars(sheetNumber=0) {
     try {
         let res = await fetchGoogleSheetData(defaultSheetIds.vernacular, sheetNumber);
-        console.log('getVernaculars RESULT:', res);
+        //console.log('getVernaculars RESULT:', res);
         if (res.status > 299) {return res;}
         let name = [];
         res.rows.forEach((row,rid) => {
             //console.log('row:', rid);
             let data = {}
-            //res.head.forEach((col,idx) => {
-            for (var i=0; i<res.head.length; i++) {
-                //console.log('head:', i, res.head[i].formattedValue);
-                //console.log('col:', i, row.values[i] ? row.values[i].formattedValue : null);
-                data[res.head[i].formattedValue] = row.values[i] ?  row.values[i].formattedValue : null;
-                //console.log('data', data);
-            }
-            if (name[data.taxonId]) {
+            /*
+            Build a JSON object using headRow keys with eachRow values like {head[i]:row.value[i], head[i+1]:row.value[i+1], ...}
+            */
+            res.head.forEach((col,idx) => { //Asynchronous loop works here! This is better UX so use it.
+            //for (var idx=0; idx<res.head.length; idx++) { let col = res.head[idx]; //synchronous loop works but delays page loading...
+                let val = row.values[idx];
+                //console.log('head:', idx, col.formattedValue);
+                //console.log('col:', idx, val ? val.formattedValue : null);
+                data[col.formattedValue] = val ? val.formattedValue : null;
+            })
+            //console.log(`Row ${rid} JSON:`, data);
+            /*
+                Now we have a JSON header-keyed row of data. Use specific keys to build a 2D Array of vernacular names like this:
+                name[taxonId][0] = {head1:row1value1, head2:row1value2, ...}
+                name[taxonId][1] = {head1:row2value1, head2:row2value2, ...}
+                name[taxonId][N] = {head1:rowNvalue1, head2:rowNvalue2, ...}
+            */
+            if (name[data.taxonId]) { //We assume that if the 1st Dimension exists, the 2nd Dimension has been instantiated, as below.
                 name[data.taxonId].push(data);
                 //console.log('duplicate', name[data.taxonId]);
             } else {
-                name[data.taxonId] = [];
+                name[data.taxonId] = []; //We must instantiate a 2D array somehow before we push values on the the 1st Dimension
                 name[data.taxonId][0] = data;
             }
         })
